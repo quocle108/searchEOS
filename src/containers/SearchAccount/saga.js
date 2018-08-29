779c96076bb31de9280bc951bed69cd018515ff4
sun1211 <ngoctam121191@yahoo.com>
2018-08-29 22:43:54 +0700

    Update saga.js

--- src/containers/SearchAccount/saga.js
+++ src/containers/SearchAccount/saga.js
@@ -2,31 +2,62 @@ import { takeLatest, call, put, select, all, fork, join } from 'redux-saga/effec
 import { makeSelectSearchName, makeSelectSearchPubkey } from './selectors';
 import { LOOKUP_ACCOUNT, LOOKUP_PUBKEY } from './constants';
 import { lookupLoading, lookupLoaded } from './actions';
+import Eos from 'eosjs';
+import {tokensUrl} from '../../remoteConfig'
 
-function* getCurrency(token, name) {
-}
+const networkOptions = {
+  broadcast: false,
+  sign: false,
+  chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+  httpEndpoint: 'https://api.eosnewyork.io:443',
+};
 
-function* getAccountDetail(name) {
-  console.log("tam_getAccountDetail ", name);
 
-}
+// function* getCurrency(token, name) {
+// }
 
 //
 // Get the EOS all accounts by public key
 //
-function* performSearchPubkey() {
+// function* performSearchPubkey() {
 
-}
+// }
+
 
 function* watchSeachPubkey() {
 }
+
+function* getAccountDetail(name) {
+  console.log("tam_getAccountDetail ", name);
+  const networkReader = yield Eos(networkOptions);
+
+  console.log("tam_ getAccountDetail_networkReader ",networkReader);
+
+  const account = yield networkReader.getAccount(name);
+  console.log("tam_ getAccountDetail_Account:", account);
+  return{
+    ...account
+  };
+}
+
+
 
 //
 // Get the EOS single account
 //
 function* performSearchAccount() {
   console.log("tam_Get the EOS single account");
+  const accountName = '123eosgui123';
+  try{
+    const account = yield call(getAccountDetail, accountName);
+    console.log("tam_return: ", account);
 
+    yield put(lookupLoaded(account));
+  } catch (err) {
+    console.error('An EOSToolkit error occured - see details below:');
+    console.error(err);
+    yield put(lookupLoaded([]));
+  }
 }
 
 function* watchSeachAccount() {
