cad72db5f3202aba61c8726afdacba3149a16087
sun1211 <ngoctam121191@yahoo.com>
2018-09-01 17:33:00 +0700

    Update showForm.js

--- src/components/showForm.js
+++ src/components/showForm.js
@@ -1,5 +1,7 @@
 /*eslint-disable no-unused-vars */
 import React from 'react'
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
 
 const showForm = props =>{
   const{account } = props;
@@ -9,8 +11,16 @@ const showForm = props =>{
 
 
     <hr />
-    <div> data: {JSON.stringify(account, null, 2)}
-    </div>
+    <Card > 
+    <Typography color="textSecondary" variant="display1" style={{ margin: 100 }}>   
+          Name     : {account.account_name} <br/>
+          EOS      : {account.core_liquid_balance} <br/>
+          RAM      : {account.ram_usage} bytes used<br/>
+                     {account.ram_quota} bytes owned <br/>                      
+          CPU      : {account.total_resources.cpu_weight} ({account.cpu_limit.used}% used) <br/>
+          NET      : {account.total_resources.net_weight} {account.net_limit.used}% used) <br/>
+    </Typography>
+    </Card>
   </div>
   )
 }
