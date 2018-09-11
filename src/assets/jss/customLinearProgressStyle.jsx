36777b6525a4532cf0cacda00162346384425b98
tam <hoang>
2018-09-11 22:53:38 +0700

    draft

---
+++ src/assets/jss/customLinearProgressStyle.jsx
@@ -0,0 +1,66 @@
+import {
+  primaryColor,
+  warningColor,
+  dangerColor,
+  successColor,
+  infoColor,
+  roseColor,
+  grayColor
+} from "./material-kit-react";
+
+const customLinearProgressStyle = {
+  root: {
+    height: "20px",
+    marginBottom: "20px",
+    overflow: "hidden",
+    borderRadius: 3,
+    borderWidth: 1,
+  },
+  bar: {
+    height: "20px"
+  },
+  primary: {
+    backgroundColor: primaryColor
+  },
+  warning: {
+    backgroundColor: warningColor
+  },
+  danger: {
+    backgroundColor: dangerColor
+  },
+  success: {
+    backgroundColor: successColor
+  },
+  info: {
+    backgroundColor: infoColor
+  },
+  rose: {
+    backgroundColor: roseColor
+  },
+  gray: {
+    backgroundColor: grayColor
+  },
+  primaryBackground: {
+    background: "rgba(156, 39, 176, 0.2)"
+  },
+  warningBackground: {
+    background: "rgba(255, 152, 0, 0.2)"
+  },
+  dangerBackground: {
+    background: "rgba(244, 67, 54, 0.2)"
+  },
+  successBackground: {
+    background: "rgba(76, 175, 80, 0.2)"
+  },
+  infoBackground: {
+    background: "rgba(0, 188, 212, 0.2)"
+  },
+  roseBackground: {
+    background: "rgba(233, 30, 99, 0.2)"
+  },
+  grayBackground: {
+    background: "rgba(221, 221, 221, 0.2)"
+  }
+};
+
+export default customLinearProgressStyle;
